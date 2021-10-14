c029207ce104639f797a03abdd05b544a52c29f7
tienthanh214 <jug@LAPTOP-DKU7IQF9.localdomain>
2021-10-14 10:16:37 +0700

    [clean dump - upload report]

--- project/nachos/NachOS-4.0/code/test/shell.c
+++ project/nachos/NachOS-4.0/code/test/shell.c
@@ -4,8 +4,8 @@ int
 main()
 {
     SpaceId newProc;
-    OpenFileId input = ConsoleInput;
-    OpenFileId output = ConsoleOutput;
+    OpenFileId input = CONSOLE_INPUT;
+    OpenFileId output = CONSOLE_OUTPUT;
     char prompt[2], ch, buffer[60];
     int i;
 
